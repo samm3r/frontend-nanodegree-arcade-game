d3225057ef2476db1ea3ecb245b9b478b35c3869
Sammer Soares <sammer.davio@gmail.com>
2018-11-22 23:16:25 -0200

    Improved code implementing classes

--- js/app.js
+++ js/app.js
@@ -1,89 +1,81 @@
+class Character {
+    constructor(sprite, x, y){
+        // this.sprite = 'images/enemy-bug.png';
+        this.sprite = sprite;
+        this.x = x;
+        this.y = y;
+        // this.speed = speed;
+    }
+
+    // Draw all characters on the screen, required method for game
+    render(){
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+
+}
+
 // Enemies our player must avoid
-var Enemy = function(y, speed) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-    this.x = -101;
-    this.y = y;
-    this.speed = speed;
-    this.player = [player.x, player.y];
-};
-
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-    var playerX = player.x;
-    console.log(playerX);
-    this.x = (this.x <= 606) ? this.x + this.speed * dt : -101;
-
-};
-
-
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-
-
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-
-var Player = function() {
-    this.sprite = 'images/char-boy.png';
-    this.x = 200;
-    this.y = 400;
-};
-
-Player.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
-
-Player.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-
-// HandleInputMethod
-Player.prototype.handleInput = function(mov) {
-    if (mov === 'right' && this.x <= 300) {
-        this.x += 100;
+class Enemy extends Character {
+    constructor(sprite, x , y, speed){
+        super(sprite, x, y);
+        this.speed = speed;
     }
-    else if(mov === 'left' && this.x >= 100){
-        this.x -= 100;
-    }
-    else if (mov === 'down' && this.y <= 317){
-        this.y += 83;
-    }
-    else if (mov === 'up' && this.y >= 68){
-        this.y -= 83;
-    }
-    this.winGame();
-};
 
-Player.prototype.winGame = function() {
-    const player = this;
-    if (this.y <= 0) {
-        setTimeout(function(){
-            alert('You win! ;)');
-            player.resetGame();
-        }, 50);
+    // Update the enemy's position, required method for game
+    // Parameter: dt, a time delta between ticks
+    update(dt){
+        this.x = (this.x <= 606) ? this.x + this.speed * dt : -101;
     }
-};
+}
 
-Player.prototype.resetGame = function() {
-    player.x = 200;
-    player.y = 400;
-};
+// Your player
+class Player extends Character {
+    constructor(sprite, x = 200, y = 400){
+        super(sprite, x, y);
+        this.score = 0;
+    }
 
+    // HandleInputMethod
+    handleInput(mov){
+        if (mov === 'right' && this.x <= 300) {
+            this.x += 100;
+        }
+        else if(mov === 'left' && this.x >= 100){
+            this.x -= 100;
+        }
+        else if (mov === 'down' && this.y <= 317){
+            this.y += 83;
+        }
+        else if (mov === 'up' && this.y >= 68){
+            this.y -= 83;
+        }
+        this.winGame();
+    }
 
+    // Update the player's position, required method for game
+    update(dt){
+
+    }
+
+    // Detect when the player wins the game
+    // and show a message on the screen
+    winGame(){
+        const player = this;
+        if (this.y <= 0) {
+            setTimeout(function(){
+                alert('You win! ;)');
+                player.resetGame();
+            }, 50);
+        }
+    }
+
+    // Send the player back to
+    // the initial position
+    resetGame(){
+        player.x = 200;
+        player.y = 400;
+    }
+}
 
 
 
@@ -91,10 +83,10 @@ Player.prototype.resetGame = function() {
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
-var bug1 = new Enemy(145, 270);
-var bug2 = new Enemy(245, 180);
+var bug1 = new Enemy('images/enemy-bug.png', -101, 145, 270);
+var bug2 = new Enemy('images/enemy-bug.png', -101, 228, 180);
 
-var player = new Player();
+var player = new Player('images/char-boy.png');
 
 var allEnemies = [bug1, bug2]
 
